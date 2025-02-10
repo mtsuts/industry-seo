c897b2df85081fe2e1833ba9e60ace64fa237a3b
mtsuts <mariamtsuts@gmail.com>
2025-02-10 00:34:11 -0800

    table data in progress

--- src/accordion.ts
+++ src/accordion.ts
@@ -6,33 +6,60 @@ d3.csv('/data/domain-rent-a-car.csv').then((data) => {
   console.log(data)
 
   const groupedData = d3.group(data, (d) => d.target)
-  
-  const updatedData = data.map((d) => {
-    
+  const uniqueTargets = Array.from(groupedData.keys())
+
+  const groupedValues = uniqueTargets.map((d: any) => {
+    console.log(groupedData.get(d))
   })
 
-
-  console.log(groupedData)
   const first50 = Array.from(new Set(data.filter((_d: any, i: any) => i <= 49)))
 
+  const accordion = d3
+    .select('.accordion')
+    .style('height', '800px')
+    .style('overflow', 'auto')
 
-  const accordion = d3.select('.accordion')
+  let isActive = true
 
-  first50.map((d) => {
+  first50.map((d, i) => {
     accordion.append('div').attr('class', 'ac').html(`<h2 class="ac-header">
       <button class="ac-trigger">
-      <div class='flex gap-36 items-center mt-6'>
+      <div class='flex gap-24 items-center mt-2'>
       <div> 
-       1
+       ${i + 1}
       </div>
-      <div class='font-normal'> ${d.target.split('.')[0]}</div>
+      <div class='font-normal flex-1'> ${d.target.split('.')[0]}</div>
       <div> Seo score</div>
-      <button> Close </button>
+      <div class='px-2 py-1 flex gap-3 items-center font-normal text-base border-2 rounded-xl ${
+        isActive
+          ? 'bg-[#29CFA8] text-white border-none'
+          : 'bg-white text-black border-[#D9D9D9]'
+      } '> 
+      <div> More Details </div>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 31 31" fill="none">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M8.53981 14.2091L10.3659 12.3831L15.5317 17.5489L20.6988 12.3831L22.5249 14.2091L15.5316 21.2024L8.53981 14.2091Z" fill="#101827"/>
+</svg>
+  
       </div>
       </button>
     </h2> 
-       <div class="ac-panel">
-      <p>Content for section 1</p>
+      <div class="ac-panel flex gap-20 text-black items-center">
+        <div class='flex gap-1 items-center'> 
+        <div class='text-sm'> LCP </div>
+        <img src='/public/black-info.svg' />
+        </div>
+
+         <div class='flex gap-1 items-center'> 
+        <div class='text-sm'> CLS </div>
+        <img src='/public/black-info.svg' />
+        </div>
+
+           <div class='flex gap-1 items-center'> 
+        <div class='text-sm'> INP </div>
+        <img src='/public/black-info.svg' />
+        </div>
+
     </div>
     `)
   })
